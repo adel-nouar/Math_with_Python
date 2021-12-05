c19115d40a9021b1157a3c1f0da0cf0359199243
Adel NOUAR <adel.nouar@protonmail.com>
2021-12-05 20:50:22 +0100

    06: Lesson plotting in Sympy

--- clear_cells.py
+++ clear_cells.py
@@ -3,4 +3,3 @@ ntbk = nbf.read("Test.ipynb", nbf.NO_CONVERT)
 new_ntbk = ntbk
 new_ntbk.cells = [cell if cell.cell_type == "markdown" else nbf.v4.new_code_cell() for cell in ntbk.cells ]
 nbf.write(new_ntbk, "Test2.ipynb", version=nbf.NO_CONVERT)
-
